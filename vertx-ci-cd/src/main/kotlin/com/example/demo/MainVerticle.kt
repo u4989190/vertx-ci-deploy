6828a78536994b06389f692d9e47ff3128d0f35a
Ysc <u4989190@anu.edu.au>
2020-08-03 23:07:44 +0900

    test promises

--- vertx-ci-cd/src/main/kotlin/com/example/demo/MainVerticle.kt
+++ vertx-ci-cd/src/main/kotlin/com/example/demo/MainVerticle.kt
@@ -2,24 +2,40 @@ package com.example.demo
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Promise
+import io.vertx.core.json.JsonObject
 
 class MainVerticle : AbstractVerticle() {
 
   override fun start(startPromise: Promise<Void>) {
-    vertx.deployVerticle(HttpVerticle())
+    //vertx.deployVerticle(HttpVerticle())
 
-    vertx.createHttpServer().requestHandler { req ->
-        req.response()
-          .putHeader("content-type", "text/plain")
-          .end("Hello from Vert.x!")
-      }
-      .listen(8888) { http ->
-        if (http.succeeded()) {
-          startPromise.complete()
-          println("HTTP server started on port 8888")
-        } else {
-          startPromise.fail(http.cause());
-        }
-      }
+    val outer: Promise<JsonObject> = Promise.promise()
+    val inner: Promise<JsonObject> = Promise.promise()
+    inner.complete()
+    inner.future().onComplete {
+      outer.complete()
+      print(it)
+    }
+
+    outer.future().onComplete{
+      print(it)
+    }
+
+
+
+
+//    vertx.createHttpServer().requestHandler { req ->
+//        req.response()
+//          .putHeader("content-type", "text/plain")
+//          .end("Hello from Vert.x!")
+//      }
+//      .listen(8888) { http ->
+//        if (http.succeeded()) {
+//          startPromise.complete()
+//          println("HTTP server started on port 8888")
+//        } else {
+//          startPromise.fail(http.cause());
+//        }
+//      }
   }
 }
